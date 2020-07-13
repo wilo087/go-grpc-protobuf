9903f1ff2f1a3cecd5fc696a8ed9dcd2e3f10cfb
Wilowayne De La Cruz G <wilo0087@gmail.com>
2020-07-13 01:27:44 -0400

    Working on server

--- server/main.go
+++ server/main.go
@@ -2,20 +2,39 @@ package main
 
 import (
 	"context"
+	"net"
 
-	"go-grpc-protobuf/proto"
+	"github.com/wilo087/grpc-protobuf/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
 
 func main() {
+	listener, err := net.Listen("tcp", "4040")
+	if err != nil {
+		panic(err)
+	}
 
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+
+	reflection.Register(srv)
 }
 
-func (S *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	a, b := request.GetA(), request.GetB()
 
 	result := a + b
+
+	return &proto.Response{Result: result}, nil
+}
+
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+
+	result := a * b
 
 	return &proto.Response{Result: result}, nil
 }
